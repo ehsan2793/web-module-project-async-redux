08f87ce6918cd14a5c022b4fa7890054dcc66626
ehsan2793 <ehsan2793@gmail.com>
2021-09-08 22:57:40 -0700

    redux has been imported and connected the the reducer function

--- app/src/index.js
+++ app/src/index.js
@@ -2,5 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import reducer from './reducer';
+import { Provider } from 'react-redux';
+const store = createStore(reducer);
+store.getState();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
---
+++ app/src/reducer/index.js
@@ -0,0 +1,12 @@
+const initialstate = {
+  name: 'ehsan',
+};
+
+const reducer = (state = initialstate, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
